5c70e5e5d38f9b33e68c1ed1115d07f587761ee1
madiportas <147020389+madiportas@users.noreply.github.com>
2026-03-21 17:29:37 -0700

    start aim 4

---
+++ R/aim_4_functional.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+# PICRUSt2 analysis (aim 4)
+
+
+# march 21st, 2026, madi + phoebe 
+# picrust analysis (R part, following module 19 tutorial) 
+# initial generation of picrust object is done on the server 
+# see PICRUSt-code.txt file 
+
+
