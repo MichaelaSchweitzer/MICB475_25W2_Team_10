b83c7ce3916212ce5026e22a4fe9ea6561df5a8b
Mia North <miasolpumpkim@gmail.com>
2026-04-19 12:56:37 -0700

    Add files via upload

--- R/Indicator Species Analysis/Indicator_Species.R
+++ R/Indicator Species Analysis/Indicator_Species.R
@@ -4,7 +4,6 @@
 library(phyloseq)
 library(tidyverse)
 library(indicspecies)
-library(ggplot2)
 
 # Load necessary objects
 load("all_phyloseq_objects_bacteriaonly_pruned.RData")
@@ -33,77 +32,6 @@ indic_res_midgut <- isa_midgut$sign %>%
 
 View(indic_res_midgut)
 
-# Hindgut analysis 
-mpt_genus_hindgut <- tax_glom(hindgut_final, "Genus", NArm = FALSE)
-mpt_genus_RA_hindgut <- transform_sample_counts(mpt_genus_hindgut, fun=function(x) x/sum(x))
-
-isa_hindgut <- multipatt(t(otu_table(mpt_genus_RA_hindgut)), 
-                         cluster = sample_data(mpt_genus_RA_hindgut)$method_gear)
-summary(isa_hindgut)
-
-taxtable_hindgut <- tax_table(hindgut_final) %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "ASV")
-
-indic_res_hindgut <- isa_hindgut$sign %>%
-  rownames_to_column(var = "ASV") %>%
-  left_join(taxtable_hindgut) %>%
-  filter(p.value < 0.05) %>%
-  pivot_longer(cols = starts_with("s."),
-               names_to = "method_gear",
-               values_to = "indicator") %>%
-  filter(indicator == 1) %>%
-  mutate(method_gear = gsub("s\\.", "", method_gear)) 
-
-View(indic_res_hindgut)
-
-# Gill analysis 
-mpt_genus_gill <- tax_glom(gill_final, "Genus", NArm = FALSE)
-mpt_genus_RA_gill <- transform_sample_counts(mpt_genus_gill, fun=function(x) x/sum(x))
-
-isa_gill <- multipatt(t(otu_table(mpt_genus_RA_gill)), 
-                         cluster = sample_data(mpt_genus_RA_gill)$method_gear)
-summary(isa_gill)
-
-taxtable_gill <- tax_table(gill_final) %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "ASV")
-
-indic_res_gill <- isa_gill$sign %>%
-  rownames_to_column(var = "ASV") %>%
-  left_join(taxtable_gill) %>%
-  filter(p.value < 0.05) %>%
-  pivot_longer(cols = starts_with("s."),
-               names_to = "method_gear",
-               values_to = "indicator") %>%
-  filter(indicator == 1) %>%
-  mutate(method_gear = gsub("s\\.", "", method_gear)) 
-
-View(indic_res_gill)
-
-# Skin analysis 
-mpt_genus_skin <- tax_glom(skin_final, "Genus", NArm = FALSE)
-mpt_genus_RA_skin <- transform_sample_counts(mpt_genus_skin, fun=function(x) x/sum(x))
-
-isa_skin <- multipatt(t(otu_table(mpt_genus_RA_skin)), 
-                      cluster = sample_data(mpt_genus_RA_skin)$method_gear)
-summary(isa_skin)
-
-taxtable_skin <- tax_table(skin_final) %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "ASV")
-
-indic_res_skin <- isa_skin$sign %>%
-  rownames_to_column(var = "ASV") %>%
-  left_join(taxtable_skin) %>%
-  filter(p.value < 0.05) %>%
-  pivot_longer(cols = starts_with("s."),
-               names_to = "method_gear",
-               values_to = "indicator") %>%
-  filter(indicator == 1) %>%
-  mutate(method_gear = gsub("s\\.", "", method_gear)) 
-
-View(indic_res_skin)
 
 # Data visualization with dot plot 
 indic_res_midgut_clean <- indic_res_midgut %>%
@@ -115,14 +43,24 @@ indic_res_midgut_clean <- indic_res_midgut %>%
 indic_res_midgut_clean %>%
   ggplot(aes(x = stat, 
              y = reorder(Genus, as.numeric(as.factor(method_gear)) * 100 + stat),
-             color = method_gear, )) +
-  geom_point() +
-  scale_color_brewer(palette = "Set1") +
+             color = method_gear)) +
+  geom_point(size = 3) +                        # increase point size (default is 1.5)
+  scale_color_manual(values = c(
+    "#E41A1C",  # red
+    "#377EB8",  # blue
+    "#4DAF4A",  # green
+    "#984EA3",  # purple
+    "#A65628",  # brown
+    "#F781BF"   # pink
+  )) +
   theme_bw() +
   theme(
     axis.text.y = element_text(face = "italic", size = 9),
     axis.text.x = element_text(size = 9),
     legend.position = "right",
+    legend.key.size = unit(1.5, "lines"),        # increase legend key size (default is 1)
+    legend.text = element_text(size = 12),       # optional: larger legend text
+    legend.title = element_text(size = 14),      # optional: larger legend title
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank()
   ) +
@@ -132,136 +70,5 @@ indic_res_midgut_clean %>%
        title = "Midgut Indicator Taxa by Gear Type")
 
 ggsave("midgut_ISA_dot.png", 
-       width = 10, 
-       height = 8)
-
-indic_res_hindgut_clean <- indic_res_hindgut %>%
-  filter(!is.na(Genus),
-         Genus != "Incertae_Sedis",
-         !grepl("Incertae", Genus)) %>%
-  mutate(Genus = gsub("g__", "", Genus))
-
-indic_res_hindgut_clean %>%
-  ggplot(aes(x = stat, 
-             y = reorder(Genus, as.numeric(as.factor(method_gear)) * 100 + stat),
-             color = method_gear, )) +
-  geom_point() +
-  scale_color_brewer(palette = "Set1") +
-  theme_bw() +
-  theme(
-    axis.text.y = element_text(face = "italic", size = 9),
-    axis.text.x = element_text(size = 9),
-    legend.position = "right",
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  ) +
-  labs(x = "Indicator Value (stat)", 
-       y = NULL,
-       color = "Gear Type",
-       title = "Hindgut Indicator Taxa by Gear Type")
-
-ggsave("hindgut_ISA_dot.png", 
-       width = 10, 
-       height = 8)
-
-indic_res_gill_clean <- indic_res_gill %>%
-  filter(!is.na(Genus),
-         Genus != "Incertae_Sedis",
-         !grepl("Incertae", Genus)) %>%
-  mutate(Genus = gsub("g__", "", Genus))
-
-indic_res_gill_clean %>%
-  ggplot(aes(x = stat, 
-             y = reorder(Genus, as.numeric(as.factor(method_gear)) * 100 + stat),
-             color = method_gear, )) +
-  geom_point() +
-  scale_color_brewer(palette = "Set1") +
-  theme_bw() +
-  theme(
-    axis.text.y = element_text(face = "italic", size = 9),
-    axis.text.x = element_text(size = 9),
-    legend.position = "right",
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  ) +
-  labs(x = "Indicator Value (stat)", 
-       y = NULL,
-       color = "Gear Type",
-       title = "Gill Indicator Taxa by Gear Type")
-
-ggsave("gill_ISA_dot.png", 
-       width = 10, 
-       height = 8)
-
-indic_res_skin_clean <- indic_res_skin %>%
-  filter(!is.na(Genus),
-         Genus != "Incertae_Sedis",
-         !grepl("Incertae", Genus)) %>%
-  mutate(Genus = gsub("g__", "", Genus))
-
-indic_res_skin_clean %>%
-  ggplot(aes(x = stat, 
-             y = reorder(Genus, as.numeric(as.factor(method_gear)) * 100 + stat),
-             color = method_gear, )) +
-  geom_point() +
-  scale_color_brewer(palette = "Set1") +
-  theme_bw() +
-  theme(
-    axis.text.y = element_text(face = "italic", size = 9),
-    axis.text.x = element_text(size = 9),
-    legend.position = "right",
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  ) +
-  labs(x = "Indicator Value (stat)", 
-       y = NULL,
-       color = "Gear Type",
-       title = "Skin Indicator Taxa by Gear Type")
-
-ggsave("skin_ISA_dot.png", 
-       width = 10, 
-       height = 8)
-
-
-
-?reorder
-
-
-
-#svg for manuscript 
-# Custom soft palette 
-custom_colors <- c(
-  "benthic trawl" = "#97bd6c",
-  "gillnet" = "#57b1e4",
-  "midwater trawl" = "#f2cd57",
-  "net" = "#dd523e",
-  "rod and reel" = "#964292",
-  "spear" = "#26436d"
-)
-
-indic_res_midgut_clean %>%
-  ggplot(aes(x = stat, 
-             y = reorder(desc(Genus), as.numeric(as.factor(method_gear)) * 100 + stat),
-             color = method_gear, )) +
-  geom_point() +
-  scale_color_manual(values = custom_colors) +
-  theme_bw() +
-  theme(
-    axis.text.y = element_text(face = "italic", size = 9),
-    axis.text.x = element_text(size = 9),
-    legend.position = "right",
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  ) +
-  labs(x = "Indicator Value (stat)", 
-       y = NULL,
-       color = "Gear Type",
-       title = "Midgut Indicator Taxa by Gear Type")
-
-ggsave("midgut_ISA_dot.svg", 
        width = 8, 
-       height = 8)
-
-ggsave("midgut_ISA_dot.png", 
-       width = 8, 
-       height = 8)
+       height = 12)
